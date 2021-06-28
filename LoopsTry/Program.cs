f93e42d57244e6d37bd370a4db547024820f3e73
LiorGrain <86430258+LiorGrain@users.noreply.github.com>
2021-06-28 17:38:03 +0300

    Add files via upload

---
+++ LoopsTry/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("num1");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("num2");
+            //int num2 = int.Parse(Console.ReadLine());
+
+
+            //do
+            //{
+            //    Console.WriteLine(num1+num2);
+            //    int num3 = num1 + num2;
+            //    switch (num3)
+            //    {
+            //        case 0:
+            //            Console.WriteLine();
+
+            //            break;
+            //    }
+            //    Console.WriteLine(num1*num2);
+            //    int num4 = num1 * num2;
+            //    break;
+            //} while (true);
+
+        }
+    }
+}
---
+++ LoopsTry/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoopsTry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoopsTry")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1aa7744e-6f29-4d39-83b8-89d7497faff9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
